155d07157d4bb28dadd7c272b403fcb91c362695
Nadika <nadika.name@gmail.com>
2024-07-10 16:56:09 +0100

    Add styles

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,29 +1,28 @@
-import React, {useState} from "react";
-import './ColorPicker.css';
+import React, { useState } from "react";
+import "./ColorPicker.css";
 
-function ColorPicker(){
+function ColorPicker() {
+  const [color, setColor] = useState("#FFF");
 
-  const [color, setColor] = useState('#FFF');
-
-  function handleColorChange(event){
+  function handleColorChange(event) {
+    console.log(event.target);
     setColor(event.target.value);
   }
 
-
-
-  return(
+  return (
     <div className="ColorPicker">
       <h1>Color Picker</h1>
-      <h2>Selected color</h2>
-      <div className="color-picker__display" style={{backgroundColor: color}}>
-
-      </div>
+      <h2>Selected color - {color}</h2>
+      <div
+        className="color-picker__display"
+        style={{ backgroundColor: color }}
+      ></div>
       <div className="color-picker-selection">
-      <h2>Select your color 😊</h2>
-      <input type="color" value={color} onChange={handleColorChange} />
+        <h2>Select your color 😊</h2>
+        <input type="color" value={color} onChange={handleColorChange} />
       </div>
     </div>
-  )
+  );
 }
 
 export default ColorPicker;
